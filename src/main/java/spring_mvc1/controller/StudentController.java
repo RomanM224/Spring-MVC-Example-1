e467426ebe887487f8fa6f38a1a4a05ee870965d
Roman <romanmaistruk@gmail.com>
2020-08-22 19:08:53 +0200

    first commit

---
+++ src/main/java/spring_mvc1/controller/StudentController.java
@@ -0,0 +1,49 @@
+package spring_mvc1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import spring_mvc1.exception.SpringException;
+import spring_mvc1.model.Student;
+
+@Controller
+public class StudentController {
+
+    @RequestMapping(value = "/student", method = RequestMethod.GET)
+    public ModelAndView student() {
+        return new ModelAndView("student.jsp", "command", new Student());
+    }
+
+//    @RequestMapping(value = "/addStudent", method = RequestMethod.POST)
+//    public String addStudent(@ModelAttribute("SpringWeb") Student student, ModelMap model) {
+//        model.addAttribute("name", student.getName());
+//        model.addAttribute("age", student.getAge());
+//        model.addAttribute("id", student.getId());
+//
+//        return "result.jsp";
+//    }
+
+    @RequestMapping(value = "/addStudent", method = RequestMethod.POST)
+    @ExceptionHandler({ SpringException.class })
+    public String addStudent(@ModelAttribute Student student, ModelMap model) {
+
+        if (student.getName().length() < 5) {
+            throw new SpringException("Given name is too short");
+        } else {
+            model.addAttribute("name", student.getName());
+        }
+
+        if (student.getAge() < 10) {
+            throw new SpringException("Given age is too low");
+        } else {
+            model.addAttribute("age", student.getAge());
+        }
+        model.addAttribute("id", student.getId());
+        return "result.jsp";
+    }
+}
---
+++ src/main/java/spring_mvc1/exception/SpringException.java
@@ -0,0 +1,19 @@
+package spring_mvc1.exception;
+
+public class SpringException extends RuntimeException {
+
+    private String exceptionMsg;
+
+    public SpringException(String exceptionMsg) {
+        this.exceptionMsg = exceptionMsg;
+    }
+
+    public String getExceptionMsg() {
+        return this.exceptionMsg;
+    }
+
+    public void setExceptionMsg(String exceptionMsg) {
+        this.exceptionMsg = exceptionMsg;
+    }
+
+}
